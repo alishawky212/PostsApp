4f87248889b74a13dc65b412bfab89c10b0cfc1a
Ali Shawky <Ali.Shawky@WARBABANK.com>
2019-07-31 00:33:34 +0300

    add unit test

--- data/src/main/java/com/example/data/remote/RemoteApis.kt
+++ data/src/main/java/com/example/data/remote/RemoteApis.kt
@@ -12,14 +12,8 @@ interface RemoteApis {
     @GET("posts/")
     fun getPosts(): Single<List<RemotePost>>
 
-    @GET("posts/{id}")
-    fun getPost(@Path("id") postId: String): Single<RemotePost>
-
     @GET("users/")
     fun getUsers(): Single<List<RemoteUser>>
-
-    @GET("users/{id}")
-    fun getUser(@Path("id") userId: String): Single<RemoteUser>
 
     @GET("comments")
     fun getPostComments(@Query("postId") postId:String):Single<List<RemoteComment>>
