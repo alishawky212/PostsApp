750372b07ae7e7cf7dd3a60adbb888fd2c1b3b8f
Ali Shawky <Ali.Shawky@WARBABANK.com>
2019-11-09 21:34:40 +0300

    fix app ktlint

--- app/src/main/java/com/example/postsapplication/master/states/PostsAction.kt
+++ app/src/main/java/com/example/postsapplication/master/states/PostsAction.kt
@@ -3,10 +3,8 @@ package com.example.postsapplication.master.states
 import com.example.android.architecture.blueprints.todoapp.mvibase.MviAction
 import com.example.postsapplication.models.PostItem
 
-sealed class PostsAction : MviAction{
-    object  LoadPostsAction : PostsAction()
+sealed class PostsAction : MviAction {
+    object LoadPostsAction : PostsAction()
 
-    data class openPostDetailAction (val post:PostItem) : PostsAction()
-
-
+    data class openPostDetailAction(val post: PostItem) : PostsAction()
 }
--- app/src/main/java/com/example/postsapplication/master/states/PostsViewState.kt
+++ app/src/main/java/com/example/postsapplication/master/states/PostsViewState.kt
@@ -4,7 +4,7 @@ import com.example.android.architecture.blueprints.todoapp.mvibase.MviViewState
 import com.example.postsapplication.models.PostItem
 
 data class PostsViewState(
-    val isLoading:Boolean,
-    val posts:List<PostItem>,
-    val error:Throwable?
+    val isLoading: Boolean,
+    val posts: List<PostItem>,
+    val error: Throwable?
 ) : MviViewState
